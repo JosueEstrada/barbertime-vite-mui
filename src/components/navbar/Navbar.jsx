22063e05ed6b8573c05a44ebdc71d5e113b8d4cb
Josué Estrada <dev.josuestrd@gmail.com>
2023-06-28 18:17:24 -0500

    Agregado drawer al navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,17 @@
 import NavbarListDrawer from "./NavbarListDrawer.jsx";
+import { Button, Drawer } from "@mui/material";
+import { useState } from "react";
 
 export default function Navbar() {
-  return <NavbarListDrawer />;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Button variant="contained" onClick={() => setIsOpen(true)}>
+        Open Drawer
+      </Button>
+      <Drawer anchor="left" open={isOpen} onClose={() => setIsOpen(false)}>
+        <NavbarListDrawer />
+      </Drawer>
+    </>
+  );
 }
