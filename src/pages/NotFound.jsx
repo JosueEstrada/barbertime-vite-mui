89929de7b32ba67707363f5e45a74a48910eb356
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-01 05:40:46 -0500

    Add functionality and visual enhancements to 404 page
    Added a redirect feature that navigates users back to the homepage if they've hit a 404. This includes a countdown timer which shows the seconds left before redirection. The redirect mechanism is accomplished with useEffect and useNavigate from react-router. Also, the page now includes enhanced UI elements using Material UI components, improving the user experience on error pages. Corregir el useNavigate por da issues

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from "react";
+import { useNavigate, Navigate, Link, NavLink } from "react-router-dom";
+import { Box, Button, Paper, Typography } from "@mui/material";
+
 export default function NotFound() {
+  const navigate = useNavigate();
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecondsLeft((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    const redirectTimer = setTimeout(() => {
+      navigate("/");
+    }, 10000);
+
+    return () => {
+      clearTimeout(redirectTimer);
+      clearInterval(timer);
+    };
+  }, [navigate]);
+
   return (
-    <>
-      <h1>404</h1>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Paper
+        elevation={10}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "1px solid black",
+          mt: 3,
+          width: "fit-content",
+          padding: 2,
+        }}
+      >
+        <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+          PÁGINA NO ENCONTRADA
+        </Typography>
+        <img
+          src={"src/assets/img/404.jpg"}
+          alt={"Imagen de Error 404 Página no encontrada"}
+          width={"200"}
+        />
+        <Typography variant="body1">
+          Redireccionando al Inicio en {secondsLeft}
+          {secondsLeft > 1 ? " segundos..." : " segundo..."}
+        </Typography>
+        <Button variant="contained" onClick={() => navigate("/")}>
+          <Typography>Vamos al Inicio</Typography>
+        </Button>
+      </Paper>
+    </Box>
   );
 }
