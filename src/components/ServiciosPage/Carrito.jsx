149523cb18fa05d305a7140c8cd2a070a3501be8
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-02 23:27:24 -0500

    Update cart component layout and adjust eslint rules
    Modified the layout of the Cart component in the Services page for improved readability and UX. The new design includes the addition of MUI components such as Box, List, and ListItem. Also added aesthetic features such as Divider and Icons for better user interaction. Additionally, the 'react/prop-types' eslint rule was turned off to avoid unnecessary warnings.

--- src/components/ServiciosPage/Carrito.jsx
+++ src/components/ServiciosPage/Carrito.jsx
@@ -1,24 +1,57 @@
-import { Grid } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Carrito({ cart, total, onRemove }) {
   return (
-    <Grid container sx={{ width: "50vw" }}>
-      <h2>Cart</h2>
-      {cart.map((service, index) => (
-        <div key={index}>
-          <p>
-            {service.name} : {service.price}
-          </p>
-          <button onClick={() => onRemove(service)}>Eliminar</button>
-        </div>
-      ))}
-      <h2>
-        Total:{" "}
-        {parseFloat(total).toLocaleString("es-PE", {
-          style: "currency",
-          currency: "PEN",
-        })}
-      </h2>
-    </Grid>
+    <>
+      <Box display="flex" justifyContent="space-around" alignItems="center">
+        <Typography variant="h6" align="center" sx={{ fontWeight: "bold" }}>
+          Carrito de Servicios
+        </Typography>
+        <ShoppingCartIcon />
+      </Box>
+      <Grid container sx={{ margin: 3 }}>
+        {cart.map((service, index) => (
+          <List key={index} dense sx={{ width: "100%" }}>
+            <ListItem>
+              <ListItemText>
+                {service.name}
+                {" : "}
+                {parseFloat(service.price).toLocaleString("es-PE", {
+                  style: "currency",
+                  currency: "PEN",
+                })}
+              </ListItemText>
+              <IconButton
+                edge="end"
+                aria-label="remover"
+                onClick={() => onRemove(service)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+            <Divider />
+          </List>
+        ))}
+
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          Total:{" "}
+          {parseFloat(total).toLocaleString("es-PE", {
+            style: "currency",
+            currency: "PEN",
+          })}
+        </Typography>
+      </Grid>
+    </>
   );
 }
