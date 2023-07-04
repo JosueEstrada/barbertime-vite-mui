fd02963d903dfcc0a5c2adee7202610be2a9592d
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-04 01:35:27 -0500

    Refactor navbar layout and remove unused icon import
    Removed the ShoppingCartCheckout.js import from Navbar.jsx as it was unused. Refactored the layout of the navbar from the Box component to have Button and nested Box components for better organization and layout control. Added styling for a hover effect on navArrayLinks to improve the user interface.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,6 @@ import {
 import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { NavLink } from "react-router-dom";
-import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout.js";
 
 const isLoggedIn = true;
 
@@ -35,48 +34,59 @@ export default function Navbar({ navArrayLinks }) {
             sx={{
               display: "flex",
               justifyContent: "space-between",
+              width: "100%",
+              alignItems: "center",
             }}
-          ></Box>
-
-          <Button
-            color="primary"
-            variant="contained"
-            sx={{
-              margin: 1,
-              marginLeft: 3,
-              width: "fit-content",
-              boxShadow: "none",
-              backgroundColor: "transparent",
-              textTransform: "none",
-            }}
-            startIcon={
-              <img
-                src="src/assets/img/logoTitle.png"
-                alt="Barbertime"
-                width="32"
-                style={{ filter: "brightness(0) invert(1)" }}
-              />
-            }
-            component={NavLink}
-            to={"/"}
           >
-            <Typography variant="h6">Barbertime</Typography>
-          </Button>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navArrayLinks.map(
-              (item) =>
-                (typeof item.requiresAuth === "undefined" ||
-                  item.requiresAuth === isLoggedIn) && (
-                  <Button
-                    color="inherit"
-                    key={item.id}
-                    component={NavLink}
-                    to={item.path}
-                  >
-                    {item.title}
-                  </Button>
-                )
-            )}
+            <Button
+              color="primary"
+              variant="contained"
+              sx={{
+                margin: 1,
+                marginLeft: 3,
+                width: "fit-content",
+                boxShadow: "none",
+                backgroundColor: "transparent",
+                textTransform: "none",
+              }}
+              startIcon={
+                <img
+                  src="src/assets/img/logoTitle.png"
+                  alt="Barbertime"
+                  width="32"
+                  style={{ filter: "brightness(0) invert(1)" }}
+                />
+              }
+              component={NavLink}
+              to={"/"}
+            >
+              <Typography variant="h6">Barbertime</Typography>
+            </Button>
+
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navArrayLinks.map(
+                (item) =>
+                  (typeof item.requiresAuth === "undefined" ||
+                    item.requiresAuth === isLoggedIn) && (
+                    <Button
+                      color="inherit"
+                      key={item.id}
+                      component={NavLink}
+                      to={item.path}
+                      sx={{
+                        marginRight: 1,
+                        "&:hover": {
+                          borderColor: "white",
+                          borderWidth: 2,
+                          borderStyle: "solid",
+                        },
+                      }}
+                    >
+                      {item.title}
+                    </Button>
+                  )
+              )}
+            </Box>
           </Box>
         </Toolbar>
       </AppBar>
