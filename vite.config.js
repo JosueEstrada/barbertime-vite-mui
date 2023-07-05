2afe9b1aa22330e7915b5ce1292ab113f271f236
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-05 03:39:50 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/JosueEstrada/barbertime-vite-mui",
+  base: "/barbertime-vite-mui/",
 });
